ef40dcb2c5969156355b9870cf5f052df31ec0c4
Krzysiek <chrisphilipniznik@gmail.com>
2020-07-18 21:37:25 +0200

    Implement method for alphabetical sort of txt file

--- src/main/java/FileWordAnalyzer.java
+++ src/main/java/FileWordAnalyzer.java
@@ -1,3 +1,9 @@
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FileWordAnalyzer {
 
     private FilePartReader filePartReader;
@@ -9,4 +15,20 @@ public class FileWordAnalyzer {
     public FilePartReader getFilePartReader() {
         return filePartReader;
     }
+
+    public List getWordsOrderedAlphabetically() throws FileNotFoundException {
+        String[] words = filePartReader.readLines().split("\\W+");
+        ArrayList<String> wordsList = new ArrayList<>(Arrays.asList(words));
+        wordsList.sort(String::compareTo);
+        return wordsList;
+    }
+
+    public List getWordsContainingSubstring() {
+        filePartReader.readLines();
+    }
+
+    public List getStringsWithPalindromes() {
+        filePartReader.readLines();
+    }
+
 }
