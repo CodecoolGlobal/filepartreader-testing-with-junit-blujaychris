5392c0f23fe34e0f00570ca502b6c3f2888d4b4f
Krzysiek <chrisphilipniznik@gmail.com>
2020-07-18 20:07:55 +0200

    Implement fields, constructor and getters/setters in FilePartReader class

--- src/main/java/FilePartReader.java
+++ src/main/java/FilePartReader.java
@@ -1,5 +1,37 @@
 public class FilePartReader {
 
+    private String filePath;
+    private int fromLine;
+    private int toLine;
 
+    public FilePartReader(String filePath, int fromLine, int toLine) {
+        this.filePath = filePath;
+        this.fromLine = fromLine;
+        this.toLine = toLine;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public int getFromLine() {
+        return fromLine;
+    }
+
+    public void setFromLine(int fromLine) {
+        this.fromLine = fromLine;
+    }
+
+    public int getToLine() {
+        return toLine;
+    }
+
+    public void setToLine(int toLine) {
+        this.toLine = toLine;
+    }
 
 }
