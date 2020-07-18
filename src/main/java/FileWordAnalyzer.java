b74930d4c4e7768d9160eabcebe85465017d5a2d
Krzysiek <chrisphilipniznik@gmail.com>
2020-07-18 21:40:46 +0200

    Implement method for returing words containing substrings

--- src/main/java/FileWordAnalyzer.java
+++ src/main/java/FileWordAnalyzer.java
@@ -3,6 +3,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileWordAnalyzer {
 
@@ -23,9 +24,15 @@ public class FileWordAnalyzer {
         return wordsList;
     }
 
-    public List getWordsContainingSubstring() {
-        filePartReader.readLines();
+    public List getWordsContainingSubstring(String subString) throws FileNotFoundException {
+        String[] words = filePartReader.readLines().split("\\W+");
+        List<String> wordsList = new ArrayList<>(Arrays.asList(words));
+        return wordsList.stream()
+                .filter(x -> x.toUpperCase().contains(subString.toUpperCase()))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
+
+
 
     public List getStringsWithPalindromes() {
         filePartReader.readLines();
