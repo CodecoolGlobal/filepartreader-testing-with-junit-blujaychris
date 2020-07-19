da2d0d562d171965b679da3c796b8bf921bf474a
Krzysiek <chrisphilipniznik@gmail.com>
2020-07-19 18:45:38 +0200

    Change name of text for alphabetic sort

--- src/test/java/FileWordAnalyzerTest.java
+++ src/test/java/FileWordAnalyzerTest.java
@@ -25,7 +25,7 @@ class FileWordAnalyzerTest {
         assertNotNull(fileWordAnalyzer.getFilePartReader());
     }
     @Test
-    public void whenWordsByABCThenReturnWordsOrderedAlphabeticallyAsArrayList() throws FileNotFoundException {
+    public void checkAlphabeticalSort() throws FileNotFoundException {
         ArrayList<String> expected = new ArrayList<>(Arrays.asList("1a", "1b", "2a", "2b", "2c","3a", "3b", "3c",
                 "3d","4a", "4b", "4c", "4d", "4e", "5a", "5b", "5c", "5d", "5e","5f", "6a", "6b", "6c", "6d",
                 "6e", "6f", "6g"));
